de9a29b4aa5ea113a1a3bbf658fc5a57447ea0cf
afiychuk <masterdream777@gmail.com>
2018-06-07 20:25:40 +0300

    last chance

--- 6381/AfiychukII/lab6/lab2.asm
+++ 6381/AfiychukII/lab6/lab2.asm
@@ -1,39 +1,25 @@
-TESTPC SEGMENT
-	ASSUME CS:TESTPC, DS:TESTPC, ES:NOTHING, SS:NOTHING
-	ORG 100H
-START:
-	JMP BEGIN
-	
-EOF	EQU '$'
-endl db 0DH,0AH,EOF
-str_inaccess_mem db 'Segment address with the first unavailable byte:', EOF
-str_inaccess_mem_empty db '     ',EOF
-str_env_address db 'Address of an environment segment:',EOF
-str_env_address_empty db '    ',EOF
-str_tail db 'Argv:',EOF
-str_tail_empty db 64 DUP(' '),EOF
-str_tail_err db 'No argv!',EOF
-str_env_content db 'Content of the environment:',0DH,0AH,EOF
-str_path db 'Path of the loaded module:',0DH,0AH,EOF
+CODE SEGMENT
+	 ASSUME CS:CODE, DS:CODE, ES:NOTHING, SS:NOTHING
+	 ORG 100H
+START: JMP BEGIN
 
-; Сокращение для функции вывода.
-PRINT_DX proc near
-	mov AH,09h
-	int 21h
-	ret
-PRINT_DX endp
-
-; Вспомогательная функция из шаблона
+; данные
+ADDR_MEM DB 'Segment address of first byte of not available memory=    ', 0AH, 0DH,'$'
+ADDR_ENV DB 'Segment address of environment=    ', 0AH, 0DH, '$'
+TAIL_OF_CMD DB 'The tail of cmd promt=' , '$'
+NEW_LINE DB  0AH, 0DH, '$'
+ENV DB 0AH, 0DH,'Enviroment:', 0AH, 0DH, '$'
+PATH_TO_PROG DB 'Path of the loaded modul=', '$'
+; процедуры
 TETR_TO_HEX PROC near
 	and AL,0Fh
 	cmp AL,09
 	jbe NEXT
 	add AL,07
-NEXT: add AL,30h
+	NEXT: add AL,30h
 	ret
 TETR_TO_HEX ENDP
 
-; байт AL переводится в два символа шестн. числа в AX
 BYTE_TO_HEX PROC near
 	push CX
 	mov AH,AL
@@ -41,16 +27,14 @@ BYTE_TO_HEX PROC near
 	xchg AL,AH
 	mov CL,4
 	shr AL,CL
-	call TETR_TO_HEX 
-	pop CX 
+	call TETR_TO_HEX ;в AL старшая цифра
+	pop CX           ;в AH младшая
 	ret
 BYTE_TO_HEX ENDP
 
-;перевод в 16 с/с 16-ти разрядного числа
-;в AX - число, DI - адрес последнего символа
-WRD_TO_HEX PROC near
-	push BX
-	mov BH,AH
+WRD_TO_HEX PROC near ;перевод в 16 с/с 16-ти разрядного числа
+	push BX          ; в AX - число, DI - адрес последнего символа
+	mov BH,AH        ;  now it aclually converts byte to string, last sybmol adress is di
 	call BYTE_TO_HEX
 	mov [DI],AH
 	dec DI
@@ -65,15 +49,14 @@ WRD_TO_HEX PROC near
 	ret
 WRD_TO_HEX ENDP
 
-;перевод в 10с/с, SI - адрес поля младшей цифры
-BYTE_TO_DEC PROC near
+BYTE_TO_DEC PROC near ; перевод байта в 10с/с, SI - адрес поля младшей цифры
+	push	AX        ; AL содержит исходный байт
 	push CX
 	push DX
 	xor AH,AH
 	xor DX,DX
 	mov CX,10
-loop_bd: 
-	div CX
+	loop_bd: div CX
 	or DL,30h
 	mov [SI],DL
 	dec SI
@@ -84,179 +67,138 @@ loop_bd:
 	je end_l
 	or AL,30h
 	mov [SI],AL
-end_l: 
-	pop DX
+	end_l: pop DX
 	pop CX
+	pop	AX
 	ret
 BYTE_TO_DEC ENDP
 
-; Печать адреса недоступной памяти
-PRINT_INACCESIBLE_MEMORY proc near
-	push ax
-	push di
-	push dx
-	
-	mov ax,ss:[2]
-	mov di, offset str_inaccess_mem_empty+3
-	call WRD_TO_HEX
-	
-	mov dx, offset str_inaccess_mem
-	call PRINT_DX
-	mov dx, offset endl
-	call PRINT_DX
-	mov dx , offset str_inaccess_mem_empty
-	call PRINT_DX
-	mov dx, offset endl
-	call PRINT_DX
-	
-	pop dx
-	pop di
-	pop ax
-	ret
-PRINT_INACCESIBLE_MEMORY ENDP
-
-; Печать сегментного адреса среды
-PRINT_ENV_ADDRES PROC near
-	push ax
-	push di
-	push dx
-	
-	mov ax,ss:[2Ch]
-	mov di, offset str_env_address_empty+3
-	call WRD_TO_HEX
-	
-	mov dx, offset str_env_address
-	call PRINT_DX
-	mov dx, offset endl
-	call PRINT_DX
-	mov dx, offset str_env_address_empty
-	call PRINT_DX
-	mov dx, offset endl
-	call PRINT_DX
-	
-	pop dx
-	pop di
-	pop ax
-	ret
-PRINT_ENV_ADDRES ENDP
-
-; Печатает аргументы командой строки
-PRINT_ARGV PROC near
-	push cx
-	push dx
-	push bx
-	
-	xor ch,ch
-	mov cl,ss:[80h]
-	
-	cmp cl,0
-	jne TailExist
-	mov dx, offset str_tail_err
-	call PRINT_DX
-	mov dx, offset endl
-	call PRINT_DX
-	pop bx
-	pop dx
-	pop cx
-	ret
-		
-TailExist:
-	mov dx, offset str_tail
-	call PRINT_DX	
-	mov bp, offset str_tail_empty
-	print_char:
-		mov di,cx
-		mov bl,ds:[di+80h]
-		mov ds:[bp+di-2],bl
-	loop print_char
-	mov dx, offset str_tail_empty
-	call PRINT_DX
-	pop bx
-	pop dx
-	pop cx
-	ret
-PRINT_ARGV ENDP
-
-; Печатает содержимое области среды
-PRINT_ENV PROC near
-	push ax
+print_addr_env proc near
+	mov dx, offset ENV
+	mov ah, 09h
+	int 21h
+	mov bx, 2Ch
+	mov ax, [bx]
+	mov ds, ax
+	mov di,0
+	mov dx, di
+	cicl:
+		cmp byte ptr [di], 0
+		je exit
+		cf:
+			inc di
+			cmp byte ptr [di], 0
+			jne cf
+			mov byte ptr [di], '$'
+				mov ah, 09h
+				int 21h
+				push dx
+				push ds
+				push es
+				mov dx, offset NEW_LINE
+				pop ds
+				int 21h
+				pop ds
+				pop dx
+			mov byte ptr [di], 0h ; восстанвливаем 0
+			inc di
+			mov dx, di
+			jmp cicl
+	exit:
 	push es
-	push bp
-	push dx
-
-	mov ax,ss:[44]
-	mov es,ax
-	xor bp,bp
-PE_cycle1:
-	cmp word ptr es:[bp],1
-	je PE_exit1
-	cmp byte ptr es:[bp],0
-	jne PE_noendl
-	mov dx, offset endl
-	call PRINT_DX
-	inc bp
-PE_noendl:
-	mov dl,es:[bp]
-	mov ah,2
+	pop ds
+	mov dx, offset NEW_LINE
+	mov ah, 09h
 	int 21h
-	inc bp
-	jmp PE_cycle1
-	
-	PE_exit1:
-	add bp,2
-	mov dx, offset endl
-	call PRINT_DX
-	mov dx, offset str_path
-	call PRINT_DX
-PE_cycle2:
-	cmp byte ptr es:[bp],0
-	je PE_exit2
-	mov dl,es:[bp]
-	mov ah,2
-	int 21h
-	inc bp
-	jmp PE_cycle2	
-PE_exit2:
-
-	pop dx
-	pop bp
-	pop es
-	pop ax
 	ret
-PRINT_ENV ENDP
+print_addr_env endp
+
+print_tail_of_cmd_str proc near
+	mov dx, offset TAIL_OF_CMD
+	mov ah, 09h
+	int 21h
+	mov bx, 80h		;число символов в строке
+	mov al, [bx] 
+	cmp al, 0
+	je empty
+		mov ah, 0
+		mov di, ax
+		mov al, [di+81h]
+		push ax
+		mov byte ptr [di+81h], '$'
+		mov dx, 81h
+		mov ah, 09h
+		int 21h
+		pop ax
+		mov [di+81h], al
+	empty:
+	mov dx, offset NEW_LINE
+	mov ah, 09h
+	int 21h
+	ret
+print_tail_of_cmd_str endp
+
+print_path_to_prog proc near ; В DI хранится адрес конца среды
+	mov dx, offset PATH_TO_PROG
+	mov ah, 09h
+	int 21h
+	mov bx, 2Ch
+	mov ax, [bx]
+	mov ds, ax
+	add di, 3
+	mov dx, di
+	find:
+	inc di
+	cmp byte ptr [di], 0
+	jne find
+	mov byte ptr [di], '$'
+		mov ah, 09h
+		int 21h
+	mov byte ptr [di], 0h ; восстанвливаем 0
+	push es
+	pop ds
+	ret
+print_path_to_prog endp
 
 BEGIN:
-	mov dx, offset endl
-	call PRINT_DX
-
-	call PRINT_INACCESIBLE_MEMORY
-	mov dx, offset endl
-	call PRINT_DX	
-
-	call PRINT_ENV_ADDRES
-	mov dx, offset endl
-	call PRINT_DX
-
-	call PRINT_ARGV
-	mov dx, offset endl
-	call PRINT_DX
-
-	mov dx, offset str_env_content
-	call PRINT_DX
-
-	call PRINT_ENV
-	mov dx, offset endl
-	call PRINT_DX
-	mov dl, 20h
-	mov ah, 02h ;вывод символа на экран
+	push DS 
+	sub AX,AX 
+	push AX 
+	
+	; 1)Сегметный адрес недоступной памяти 
+	mov bx, 2h
+	mov ax, [bx]
+	mov DI, OFFSET (ADDR_MEM+57)
+	call WRD_TO_HEX
+	mov dx, offset ADDR_MEM
+	mov ah, 09h
 	int 21h
+	
+	; 2)Сегметный адрес среды
+	mov bx, 2ch
+	mov ax, [bx]
+	mov DI, OFFSET (ADDR_ENV+34)
+	call WRD_TO_HEX
+	mov dx, offset ADDR_ENV
+	mov ah, 09h
+	int 21h
+
+	; 3)Хвост командной строки
+	call print_tail_of_cmd_str
+	
+	; 4)Содержимое области среды
+	call print_addr_env
+	
+	; 5)Путь загружаемого модуля
+	call print_path_to_prog
 	
 	mov AH, 01h ;Клавиатурный ввод символа (код символа помещается в регистр AL)
 	int 21h
-					
-; выход в DOS
-	mov ah, 4ch
-	int 21h
+	;mov AH, 01h ;Клавиатурный ввод символа (код символа помещается в регистр AL)
+	;int 21h
 	
-TESTPC ENDS
- END START
+	; Выход в DOS
+	mov AH,4Ch
+	int 21H
+CODE ENDS
+END START
--- 6381/AfiychukII/lab6/lab6.asm
+++ 6381/AfiychukII/lab6/lab6.asm
@@ -1,252 +1,246 @@
+STACK SEGMENT STACK
+	DW 64 DUP (?)
+STACK ENDS
+;---------------------------------------------------------------
+DATA SEGMENT
+ParameterBlock dw ? ;сегментный адрес среды
+	dd ? ;сегмент и смещение командной строки
+	dd ? ;сегмент и смещение первого FCB
+	dd ? ;сегмент и смещение второго FCB
+	Mem_7    DB 0DH, 0AH,'Memory control unit destroyed!',0DH,0AH,'$'
+	Mem_8    DB 0DH, 0AH,'Not enough memory to perform the function!',0DH,0AH,'$'
+	Mem_9    DB 0DH, 0AH,'Wrong address of the memory block!',0DH,0AH,'$'
+	Err_1    DB 0DH, 0AH,'The number of function is wrong!',0DH,0AH,'$'
+	Err_2    DB 0DH, 0AH,'File not found!',0DH,0AH,'$'
+	Err_5    DB 0DH, 0AH,'Disk error!',0DH,0AH,'$'
+	Err_8    DB 0DH, 0AH,'Insufficient value of memory!',0DH,0AH,'$'
+	Err_10   DB 0DH, 0AH,'Incorrect environment string!',0DH,0AH,'$'
+	Err_11   DB 0DH, 0AH,'Wrong format!',0DH,0AH,'$'
+	End_0    DB 0DH, 0AH,'Normal termination!',0DH,0AH,'$'
+	End_1    DB 0DH, 0AH,'End by Ctrl-Break!',0DH,0AH,'$'
+	End_2    DB 0DH, 0AH,'The completion of the device error!',0DH,0AH,'$'
+	End_3    DB 0DH, 0AH,'Completion by function 31h!',0DH,0AH,'$'
+	PATH 	 DB '                                               ',0DH,0AH,'$',0
+	KEEP_SS  DW 0
+	KEEP_SP  DW 0
+	END_CODE DB 'End code:   ',0DH,0AH,'$'
+DATA ENDS
+;---------------------------------------------------------------
 CODE SEGMENT
- ASSUME CS:CODE, DS:DATA, ES:DATA, ss:STACK
-
-START: jmp MAIN
-
-; Сокращение для функции вывода.
-PRINT_DX proc near
-	mov ah,09h
+ ASSUME CS:CODE, DS:DATA, ES:DATA, SS:STACK
+START: JMP MAIN
+;ПРОЦЕДУРЫ
+;---------------------------------------------------------------
+PRINT PROC NEAR ;печать на экран 
+	push ax
+	mov ah, 09h
 	int 21h
+	pop ax
 	ret
-PRINT_DX endp
-
-
-; Половина байт AL переводится в символ шестнадцатиричного числа в AL
-TETR_TO_HEX	proc near 
-    and	al, 0Fh
-    cmp	al, 09
-    jbe	NEXT 
-    add	al, 07
-NEXT:	
-    add		al, 30h
-    ret
-TETR_TO_HEX	endp
-
-
-; Байт AL переводится в два символа шестнадцатиричного числа в AX
-BYTE_TO_HEX	proc near 
-    push cx
-    mov	ah, al 
-    call TETR_TO_HEX
-    xchg al, ah 
-    mov	cl, 4 
-    shr	al, cl 
-    call TETR_TO_HEX 
-    pop	cx 			
-    ret
-BYTE_TO_HEX	endp
-
-
-INIT_PARAM_DATA_BLOCK proc
-	mov ax, es
-	mov ParamDataBlock,0
-	mov ParamDataBlock+2, ax
-	mov ParamDataBlock+4, 80h
-	mov ParamDataBlock+6, ax
-	mov ParamDataBlock+8, 5Ch
-	mov ParamDataBlock+10, ax
-	mov ParamDataBlock+12, 6Ch
-	ret
-INIT_PARAM_DATA_BLOCK endp
-
-
-; Освобождение места в памяти
-FREE_MEM proc 
-	mov bx, offset LAST_BYTE
-	mov ax, es ;es-начало
-	sub bx, ax
-	mov cl, 4h
-	shr bx, cl
-	; в BX количество параграфов, которые будут выделяться модулю
-
-	mov ah,4Ah 
+PRINT ENDP
+;--------------------------------------------------------------------------------
+TETR_TO_HEX		PROC near ;половина байт AL переводится в символ шестнадцатиричного числа в AL
+		and		al, 0Fh ;and 00001111 - оставляем только вторую половину al
+		cmp		al, 09 ;если больше 9, то надо переводить в букву
+		jbe		NEXT ;выполняет короткий переход, если первый операнд МЕНЬШЕ или РАВЕН второму операнду
+		add		al, 07 ;дополняем код до буквы
+	NEXT:	add		al, 30h ;16-ричный код буквы или цифры в al
+		ret
+TETR_TO_HEX		ENDP
+;--------------------------------------------------------------------------------
+BYTE_TO_HEX		PROC near ;байт AL переводится в два символа шестнадцатиричного числа в AX
+		push	cx
+		mov		ah, al ;копируем al в ah
+		call	TETR_TO_HEX ;переводим al в символ 16-рич.
+		xchg	al, ah ;меняем местами al и  ah
+		mov		cl, 4 
+		shr		al, cl ;cдвиг всех битов al вправо на 4
+		call	TETR_TO_HEX ;переводим al в символ 16-рич.
+		pop		cx 			
+		ret
+BYTE_TO_HEX		ENDP
+;---------------------------------------------------------------
+FreeSpaceInMemory PROC ;подготовка и освобождение места в памяти 
+	;перед вызовом функции надо определить объём памяти, необходимый программе ЛР6 
+	;и задать в регистре BX число параграфов, которые будут выделяться прграмме
+	mov bx,offset LAST_BYTE ;кладём в ax адрес конца программы
+	mov ax,es ;es-начало
+	sub bx,ax ;bx=Размер=Конец-начало
+	mov cl,4h
+	shr bx,cl ;переводим в параграфы
+	;освобождаем место в памяти
+	mov ah,4Ah ;функция позволяет уменьшить отведённый программе блок памяти
 	int 21h
-	jnc NO_ERROR ; CF=0 при отсутствии ошибок
+	jnc NO_ERROR ;CF=0 - если нет ошибки
 	
-	;oбработка ошибок (если CF != 0, то в AX  - код ошибки)
-	cmp ax, 7 
-	mov dx, offset str_err_mcb_damaged
-	je IS_ERROR
-	cmp ax, 8 
-	mov dx, offset str_err_addr_incorrect
-	je IS_ERROR
-	cmp ax, 9 
-	mov dx, offset str_err_addr_incorrect
+	;oбработка ошибок CF=1 AX = код ошибки если CF установлен 
+	cmp ax,7 ;разрушен управляющий блок памяти
+	mov dx,offset Mem_7
+	je YES_ERROR
+	cmp ax,8 ;недостаточно памяти для выполнения функции
+	mov dx,offset Mem_8
+	je YES_ERROR
+	cmp ax,9 ;неверный адрес блока памяти
+	mov dx,offset Mem_9
 	
-IS_ERROR:
-	call PRINT_DX
+YES_ERROR:
+	call PRINT ;выводим ошибку на экран
 	xor al,al
 	mov ah,4Ch
 	int 21H
 NO_ERROR:
 	ret
-FREE_MEM endp
-
-
-RUN_PROCESS proc 	
-	mov es, es:[2Ch]
+FreeSpaceInMemory ENDP
+;---------------------------------------------------------------
+CreateBlockOfParameter PROC ;заполнение блока параметров
+	mov ax, es
+	mov ParameterBlock,0 ;сегментный адрес среды
+	mov ParameterBlock+2, ax ;сегмент командной строки
+	mov ParameterBlock+4, 80h ;смещение командной строки
+	mov ParameterBlock+6, ax ;сегмент первого FCB
+	mov ParameterBlock+8, 5Ch ;смещение первого FCB
+	mov ParameterBlock+10, ax ;сегмент второго FCB
+	mov ParameterBlock+12, 6Ch ;смещение второго FCB
+	ret
+CreateBlockOfParameter ENDP
+;---------------------------------------------------------------
+Calling PROC 
+	
+	
+;-----------------------------------------	
+	
+	mov es, es:[2Ch]; сегментный адрес среды, передаваемый программе
 	mov si, 0
-
-HANDLE_ENVIR:
+env:
 	mov dl, es:[si]
-	cmp dl, 00
-	je DO_END_OF_LINE	
+	cmp dl, 00h		; конец строки?
+	je EOL_	
 	inc si
-	jmp HANDLE_ENVIR
-
-DO_END_OF_LINE:
+	jmp env
+EOL_:
 	inc si
 	mov dl, es:[si]
-	cmp dl, 00
-	jne HANDLE_ENVIR	
-	add si, 03
+	cmp dl, 00h		;конец среды?
+	jne env
+	
+	add si, 03h	; si указывает на начало маршрута	
+	
 	push di
 	lea di, PATH
-
-HANDLE_PATH:
+path_:
 	mov dl, es:[si]
-	cmp dl, 00
-	je DO_END_OF_LINE_AGAIN	
+	cmp dl, 00h		;конец маршрута?
+	je EOL2	
 	mov [di], dl	
 	inc di			
-	inc si	
-	jmp HANDLE_PATH
-
-DO_END_OF_LINE_AGAIN:
+	inc si			
+	jmp path_
+EOL2:
 	sub di, 05h	
 	mov [di], byte ptr '2'	
 	mov [di+2], byte ptr 'C'
 	mov [di+3], byte ptr 'O'
 	mov [di+4], byte ptr 'M'
 	mov [di+5], byte ptr 0h
+	
 	pop di
+;-----------------------------------------
+	;сохраняем содержимое регистров SS и SP в переменных
 	mov KEEP_SP, SP
 	mov KEEP_SS, SS
+	
+	;на блок параметров перед загрузкой вызываемой программы должны указывать ES:BX
 	push ds
-	pop es
-	mov bx, offset ParamDataBlock
-	mov dx, offset PATH
-	mov ax, 4B00h
+	pop es ;es - сегмент данных
+	mov bx,offset ParameterBlock
+	
+	;подготовить строку, содержащую путь и имя вызываемой программы
+	;на подготовленную строку должны указазывать DS:DX
+	mov dx,offset PATH
+	
+	;вызываем загрузчик OS
+	mov ax,4B00h
 	int 21h
-	jnc IS_LOADED
+	jnc IS_LOADED ;если вызываемая программа не была загружена, 
+	;то устанавливается флаг переноса CF=1 и в AX заносится код ошибки
 	
+	;восстанавление DS, SS, SP
 	push ax
-	mov ax, DATA
-	mov ds, ax
+	mov ax,DATA
+	mov ds,ax
 	pop ax
-	mov SS, KEEP_SS
-	mov SP, KEEP_SP
+	mov SS,KEEP_SS
+	mov SP,KEEP_SP
 	
-	cmp ax, 1
-	mov dx, offset str_err_func_number_incorrect
-	je SIMPLE_EXIT
-
-	cmp ax, 2
-	mov dx, offset str_err_file_not_found
-	je SIMPLE_EXIT
-
-	cmp ax, 5
-	mov dx, offset str_err_disk
-	je SIMPLE_EXIT
-
-	cmp ax, 8
-	mov dx, offset str_err_mem_val_incorrect
-	je SIMPLE_EXIT
-
-	cmp ax, 10
-	mov dx, offset str_err_env_val_incorrect
-	je SIMPLE_EXIT
-
-	cmp ax, 11
-	mov dx, offset str_err_format_incorrect
-	je SIMPLE_EXIT
+	;обработка ошибок
+	cmp ax,1 ;если номер функции неверен
+	mov dx,offset Err_1
+	je EXIT1
+	cmp ax,2 ;если файл не найден
+	mov dx,offset Err_2
+	je EXIT1
+	cmp ax,5 ;при ошибке диска
+	mov dx,offset Err_5
+	je EXIT1
+	cmp ax,8 ;при недостаточном объёме памяти
+	mov dx,offset Err_8
+	je EXIT1
+	cmp ax,10 ;при неправильной строке среды
+	mov dx,offset Err_10
+	je EXIT1
+	cmp ax,11 ;если неверен формат
+	mov dx,offset Err_11
 	
-SIMPLE_EXIT:
-	call PRINT_DX
+EXIT1:
+	call PRINT
 	
-	xor al ,al
-	mov ah, 4Ch
+	;выходим в DOS
+	xor al,al
+	mov ah,4Ch
 	int 21h
 		
-IS_LOADED:
-	mov ax,4d00h
+IS_LOADED: ;если CF=0, то вызываемая программа выполнена
+	mov ax,4d00h ;в AH - причина, в AL - код завершения
 	int 21h
 	
-	cmp ah, 0
-	mov dx, offset str_end_normal
-	je EXIT_WITH_MES
-	cmp ah, 1
-	mov dx, offset str_end_ctrl_end
-	je EXIT_WITH_MES
-	cmp ah, 2
-	mov dx, offset str_err_device
-	je EXIT_WITH_MES
-	cmp ah, 3
-	mov dx, offset str_end_31h
+	;обработка завершения программы
+	cmp ah,0 ;нормальное завершение
+	mov dx,offset End_0
+	je EXIT2
+	cmp ah,1 ;завершение по Ctrl-Break
+	mov dx,offset End_1
+	je EXIT2
+	cmp ah,2 ;завершение по ошибке устройства
+	mov dx,offset End_2
+	je EXIT2
+	cmp ah,3 ;завершение по функции 31h, оставляющей программу резидентной
+	mov dx,offset End_3
 
-EXIT_WITH_MES:
-	call PRINT_DX
-	mov di, offset END_CODE
+EXIT2:
+	call PRINT
+
+	;выводим код завершения на экран
+	mov di,offset END_CODE
 	call BYTE_TO_HEX
-	add di, 0Ah
-	mov [di], al
-	add di, 1
-	xchg ah, al
-	mov [di], al
+	add di,0Ah
+	mov [di],al
+	add di,1h
+	xchg ah,al
+	mov [di],al
 	mov dx, offset END_CODE
-	call PRINT_DX
+	call PRINT
 	ret
-RUN_PROCESS ENDP
-
-
+Calling ENDP
+;---------------------------------------------------------------
 MAIN:
-	mov ax, DATA
-	mov ds, ax
-	call FREE_MEM 
-	call INIT_PARAM_DATA_BLOCK
-	call RUN_PROCESS
-
+	mov AX,DATA
+	mov DS,AX
+	call FreeSpaceInMemory 
+	call CreateBlockOfParameter
+	call Calling
 	xor al,al
 	mov ah,4Ch ;выход 
 	int 21h
-
 LAST_BYTE:
-CODE ENDS
-
-
-DATA SEGMENT
-    ParamDataBlock    	dw ? ;сегментный адрес среды
-                        dd ? ;сегмент и смещение командной строки
-                        dd ? ;сегмент и смещение первого FCB
-                        dd ? ;сегмент и смещение второго FCB
-    ; end_of_param_block
-
-	str_err_mcb_damaged             DB 0DH, 0AH, 'Memory control unit has been damaged!',0DH,0AH,'$'
-	str_err_func_number_incorrect   DB 0DH, 0AH, 'The number of function is incorrect!',0DH,0AH,'$'
-	str_err_not_enough_mem          DB 0DH, 0AH, 'Not enough memory to perform the function!',0DH,0AH,'$'
-	str_err_addr_incorrect          DB 0DH, 0AH, 'Incorrect address of the memory block!',0DH,0AH,'$'
-	str_err_disk                    DB 0DH, 0AH, 'Disk reading error!',0DH,0AH,'$'
-	str_err_file_not_found          DB 0DH, 0AH, 'File not found!',0DH,0AH,'$'
-	str_err_format_incorrect        DB 0DH, 0AH, 'Incorrect format!',0DH,0AH,'$'
-	str_err_mem_val_incorrect       DB 0DH, 0AH, 'Incorrect value of memory!',0DH,0AH,'$'
-	str_err_device                  DB 0DH, 0AH, 'Completion-device error!',0DH,0AH,'$'
-	str_err_env_val_incorrect       DB 0DH, 0AH, 'Incorrect environment string!',0DH,0AH,'$'
-    ; end_of_errors_sector
-
-	str_end_ctrl_end                DB 0DH, 0AH, 'Module has beem ended by Ctrl-Break!',0DH,0AH,'$'
-	str_end_normal                  DB 0DH, 0AH, 'Module has been ended is normal way!',0DH,0AH,'$'
-	str_end_31h                     DB 0DH, 0AH, 'Completion by function 31h!',0DH,0AH,'$'
-    ; end_of_endings_sector
-
-	PATH 	 DB '                                               ',0DH,0AH,'$',0
-	KEEP_SS  DW 0
-	KEEP_SP  DW 0
-	END_CODE DB 'End code:   ',0DH,0AH,'$'
-    ; end_of_help_sector
-DATA ENDS
-
-STACK SEGMENT STACK
-	DW 64 DUP (?)
-STACK ENDS
-
-END START
+	CODE ENDS
+	END START
